97a11a67573e2107f26bc6c5bcbce084b034d9ff
JevonYang <yangzijing08@gmail.com>
2018-06-27 10:58:39 +0800

    门面模式FacadePattern

---
+++ src/com/yang/Facade.java
@@ -0,0 +1,52 @@
+package com.yang;
+
+interface Process{
+    public void step1();
+
+    public void step2();
+
+    public void step3();
+}
+
+class ProcessImp implements Process{
+    @Override
+    public void step1() {
+        System.out.println("hello, here is step 1");
+    }
+
+    @Override
+    public void step2() {
+        System.out.println("hello, here is step 2");
+    }
+
+    @Override
+    public void step3() {
+        System.out.println("hello, here is step 3");
+    }
+}
+
+class MyFacde{
+    private Process process;
+
+    public MyFacde(Process process) {
+        this.process = process;
+    }
+
+    public void myProcess(){
+        this.process.step1();
+        this.process.step2();
+        this.process.step3();
+    }
+
+
+}
+
+public class Facade {
+
+    public static void main(String[] args) {
+        Process process = new ProcessImp();
+        MyFacde myFacde = new MyFacde(process);
+        myFacde.myProcess();
+    }
+
+}
